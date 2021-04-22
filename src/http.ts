718821d400c679e1a78c7e928e308db0cf830676
Nathan <nathannss14@gmail.com>
2021-04-22 18:45:58 -0300

    Addition of WebSocket, creation of the last table and implementation of the application's FrontEnd

---
+++ src/http.ts
@@ -0,0 +1,39 @@
+import express from 'express';
+import { createServer } from "http";
+import { Server, Socket } from "socket.io";
+import path from "path";
+
+import "./database";
+import { routes } from "./routes";
+
+/**
+ * GET = Buscas
+ * POST = Criação
+ * PUTH = Alteração
+ * DELETE = DELETAR
+ * PATCH = Alterar uma Informação Especifica 
+ */
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "..", "public")));
+app.set("views", path.join(__dirname, "..", "public"));
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "html");
+
+app.get("/pages/client", (request, response)=>{
+    return response.render("html/client.html");
+});
+
+const http = createServer(app);
+const io = new Server(http);
+
+io.on("connection", (socket: Socket) => {
+    console.log("Se Conectou", socket.id);
+});
+
+app.use(express.json());
+
+app.use(routes);
+
+export {http, io};
--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,4 @@
-import express from 'express';
+import {http} from "./http";
+import "./websocket/client";
 
-import "./database";
-
-import { routes } from "./routes";
-
-/**
- * GET = Buscas
- * POST = Criação
- * PUTH = Alteração
- * DELETE = DELETAR
- * PATCH = Alterar uma Informação Especifica 
- */
-
-const app = express();
-
-app.use(express.json());
-
-app.use(routes);
-
-app.listen(3333, () => console.log('Ta Rodando :)'));
+http.listen(3333, () => console.log('Ta Rodando :)'));
